7542c278958d4504f732e2b4a878402f50faca78
DT5642 <jackspeers@me.com>
2025-02-27 14:03:24 +1100

    Completed Pt.6: Functions

--- Zorp/main.cpp
+++ Zorp/main.cpp
@@ -18,55 +18,172 @@ const char* INDENT = "\x1b[5C";
 const char* YELLOW = "\x1b[93m";
 const char* MAGENTA = "\x1b[95m";
 const char* RESET_COLOR = "\x1b[0m";
-const char* SAVE_CURSOR_POS = "\x1b[s";
-const char* RESTORE_CURSOR_POS = "\x1b[u";
+
+const int EMPTY = 0;
+const int ENEMY = 1;
+const int TREASURE = 2;
+const int FOOD = 3;
+const int ENTRANCE = 4;
+const int EXIT = 5;
+
+const int MAX_RANDOM_TYPE = FOOD + 1;
+
+const int MAZE_WIDTH = 10;
+const int MAZE_HEIGHT = 6;
+
+const int INDENT_X = 5;
+const int ROOM_DESC_Y = 8;
+const int MOVEMENT_DESC_Y = 9;
+const int MAP_Y = 13;
+const int PLAYER_INPUT_X = 30;
+const int PLAYER_INPUT_Y = 11;
+
+const int WEST = 4;
+const int EAST = 6;
+const int NORTH = 8;
+const int SOUTH = 2;
+
+bool EnableVirtualTerminal();
+void Initialize(int map[MAZE_HEIGHT][MAZE_WIDTH]);
+void DrawWelcomeMessage();
+void DrawRoom(int map[MAZE_HEIGHT][MAZE_WIDTH], int x, int y);
+void DrawMap(int map[MAZE_HEIGHT][MAZE_WIDTH]);
+void DrawRoomDescription(int roomType);
+void DrawPlayer(int x, int y);
+void DrawValidDirections(int x, int y);
 
 void main()
 {
-	const int EMPTY = 0;
-	const int ENEMY = 1;
-	const int TREASURE = 2;
-	const int FOOD = 3;
-	const int ENTRANCE = 4;
-	const int EXIT = 5;
-
-	const int MAX_RANDOM_TYPE = FOOD + 1;
-
-	const int MAZE_WIDTH = 10;
-	const int MAZE_HEIGHT = 6;
-
-	const int INDENT_X = 5;
-	const int ROOM_DESC_Y = 8;
-	const int MAP_Y = 13;
-	const int PLAYER_INPUT_X = 30;
-	const int PLAYER_INPUT_Y = 11;
-
-	const int WEST = 4;
-	const int EAST = 6;
-	const int NORTH = 8;
-	const int SOUTH = 2;
+	//Create 2D array
+	int rooms[MAZE_HEIGHT][MAZE_WIDTH];
 
 	bool gameOver = false;
 	int playerX = 0;
 	int playerY = 0;
 
-	int height = 0;
-	int avatarHP = 0;
-	char firstLetterOfName = 0;
+	if (EnableVirtualTerminal() == false)
+	{
+		cout << "The virtual terminal processing mode could not be activated.\n";
+		cout << "Press 'Enter' to exit.\n";
+		cin.get();
+		return;
+	}
 
+	Initialize(rooms);
+
+	DrawWelcomeMessage();
+	cin.get();
+
+	//Output map
+	DrawMap(rooms);
+
+	//Game loop
+	while (!gameOver)
+	{
+		DrawRoomDescription(rooms[playerY][playerX]);
+
+		DrawPlayer(playerX, playerY);
+
+		if (rooms[playerY][playerX] == EXIT)
+		{
+			gameOver = true;
+			continue;
+		}
+
+		//List the directions the player can take
+		DrawValidDirections(playerX, playerY);
+		int direction = GetMovementDirection();
+
+		//Before updating the player position, redraw the old room
+		//Character over the old position
+
+		DrawRoom(rooms, playerX, playerY);
+
+		//Update the player's position using the input movement data
+		switch (direction)
+		{
+		case EAST:
+			{
+				if (playerX < MAZE_WIDTH - 1)
+				{
+					playerX++;
+					break;
+				}
+			}
+		case WEST:
+			{
+				if (playerX > 0)
+				{
+					playerX--;
+					break;
+				}
+			}
+		case NORTH:
+			{
+				if (playerY > 0)
+				{
+					playerY--;
+					break;
+				}
+			}
+		case SOUTH:
+			{
+				if (playerY < MAZE_HEIGHT - 1)
+				{
+					playerY++;
+					break;
+				}
+			}
+		default:
+			{
+				//The direction was not valid,
+				//Do nothing, go back to the top of the loop and ask again
+				break;
+			}
+		}
+	}
+	//End of game loop
+
+	cout << CSI << PLAYER_INPUT_Y << ";" << 0 << "H";
+	cout << "\n" << INDENT << "Press 'Enter' to exit the program.\n";
+	cin.clear();
+	cin.ignore(cin.rdbuf()->in_avail());
+	cin.get();
+
+	return;
+}
+
+bool EnableVirtualTerminal()
+{
 	//Set output mode to handle virtual terminal sequences
-	DWORD dwMode = 0;
 	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-	GetConsoleMode(hOut, &dwMode);
+
+	if (hOut == INVALID_HANDLE_VALUE)
+	{
+		return false;
+	}
+
+	DWORD dwMode = 0;
+
+	if (!GetConsoleMode(hOut, &dwMode))
+	{
+		return false;
+	}
+
 	dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
-	SetConsoleMode(hOut, dwMode);
+	if (!SetConsoleMode(hOut, dwMode))
+	{
+		return false;
+	}
 
-	//Create 2D array
-	int rooms[MAZE_HEIGHT][MAZE_WIDTH];
+	return true;
+}
 
+void Initialize(int map[MAZE_HEIGHT][MAZE_WIDTH])
+{
 	srand(time(nullptr));
 
-	//Fill the awways with room types
+	//Fill the arrays with random room types
 	for (int y = 0; y < MAZE_HEIGHT; y++)
 	{
 		for (int x = 0; x < MAZE_WIDTH; x++)
@@ -75,231 +192,182 @@ void main()
 
 			if (type < MAX_RANDOM_TYPE)
 			{
-				rooms[y][x] = type;
+				map[y][x] = type;
 			}
 			else
 			{
-				rooms[y][x] = EMPTY;
+				map[y][x] = EMPTY;
 			}
 		}
 	}
 
 	//Set the entrance and exit of the maze
-	rooms[0][0] = ENTRANCE;
-	rooms[MAZE_HEIGHT - 1][MAZE_WIDTH - 1] = EXIT;
+	map[0][0] = ENTRANCE;
+	map[MAZE_HEIGHT - 1][MAZE_WIDTH - 1] = EXIT;
+}
 
-	cout << "Hello world!\n\n";
-
+void DrawWelcomeMessage()
+{
 	cout << TITLE << MAGENTA << "Welcome to Zorp!\n" << RESET_COLOR;
 	cout << INDENT << "Zorp is a game of adventure, danger, and low cunning.\n";
-	cout << INDENT << "It is definitely not related to any other text-based adventure game...\n";
-	cin.get();
+	cout << INDENT << "It is definitely not related to any other text-based adventure game...\n\n";
+}
 
-	//Save cursor position
-	cout << SAVE_CURSOR_POS;
+void DrawRoom(int map[MAZE_HEIGHT][MAZE_WIDTH], int x, int y)
+{
+	//Find the console output position
+	int outX = INDENT_X + (6 * x) + 1;
+	int outY = MAP_Y + y;
 
-	//Output map
-	cout << CSI << MAP_Y << ";" << 0 << "H";
+	//Jump to the correct loction
+	cout << CSI << outY << ";" << outX << "H";
+
+	//Draw the room
+	switch (map[y][x])
+	{
+	case EMPTY:
+		{
+			cout << "[ " << GREEN << "\xb0" << RESET_COLOR << " ] ";
+			break;
+		}
+	case ENEMY:
+		{
+			cout << "[ " << RED << "\x94" << RESET_COLOR << " ] ";
+			break;
+		}
+	case TREASURE:
+		{
+			cout << "[ " << YELLOW << "$" << RESET_COLOR << " ] ";
+			break;
+		}
+	case FOOD:
+		{
+			cout << "[ " << WHITE << "\xcf" << RESET_COLOR << " ] ";
+			break;
+		}
+	case ENTRANCE:
+		{
+			cout << "[ " << WHITE << "\x9d" << RESET_COLOR << " ] ";
+			break;
+		}
+	case EXIT:
+		{
+			cout << "[ " << WHITE << "\xFE" << RESET_COLOR << " ] ";
+			break;
+		}
+	}
+}
+
+void DrawMap(int map[MAZE_HEIGHT][MAZE_WIDTH])
+{
+	//Reset draw colours
+	cout << RESET_COLOR;
+
 	for (int y = 0; y < MAZE_HEIGHT; y++)
 	{
 		cout << INDENT;
 		for (int x = 0; x < MAZE_WIDTH; x++)
 		{
-			switch (rooms[y][x])
-			{
-			case EMPTY:
-			{
-				cout << "[ " << GREEN << "\xb0" << RESET_COLOR << " ] ";
-				break;
-			}
-			case ENEMY:
-			{
-				cout << "[ " << RED << "\x94" << RESET_COLOR << " ] ";
-				break;
-			}
-			case TREASURE:
-			{
-				cout << "[ " << YELLOW << "$" << RESET_COLOR << " ] ";
-				break;
-			}
-			case FOOD:
-			{
-				cout << "[ " << WHITE << "\xcf" << RESET_COLOR << " ] ";
-				break;
-			}
-			case ENTRANCE:
-			{
-				cout << "[ " << WHITE << "\x9d" << RESET_COLOR << " ] ";
-				break;
-			}
-			case EXIT:
-			{
-				cout << "[ " << WHITE << "\xFE" << RESET_COLOR << " ] ";
-				break;
-			}
-			}
+			DrawRoom(map, x, y);
 		}
 		cout << "\n";
 	}
+}
 
-	//Game loop
-	while (!gameOver)
+void DrawRoomDescription(int roomType)
+{
+	//Reset draw colours
+	cout << RESET_COLOR;
+
+	//Jump to the correct location
+	cout << CSI << ROOM_DESC_Y << ";" << 0 << "H";
+
+	//Delete 4 lines and insert 4 empty lines
+	cout << CSI << "4M" << CSI << "4L\n";
+
+	//Write description of current room
+	switch (roomType)
 	{
-		//Prepare screen for output
-		//Move cursor to start of the 1st Q, then up 1, delete and insert 4 lines
-		cout << RESTORE_CURSOR_POS << CSI << "A" << CSI << "4M" << CSI << "4L" << "\n";
-
-		//Write description of current room
-		switch (rooms[playerY][playerX])
+	case EMPTY:
 		{
-		case EMPTY:
-			{
-				cout << INDENT << "You are in an empty meadow. There is nothing of note here.\n";
-				break;
-			}
-		case ENEMY:
-			{
-				cout << INDENT << "BEWARE. An enemy is approaching.\n";
-				break;
-			}
-		case TREASURE:
-			{
-				cout << INDENT << "Your journey has been rewarded. You have found some treasure.\n";
-				break;
-			}
-		case FOOD:
-			{
-				cout << INDENT << "At last! You collect some food to sustain you on your journey.\n";
-				break;
-			}
-		case ENTRANCE:
-			{
-				cout << INDENT << "The entrance you used to enter this maze is blocked. There is no going back.\n";
-				break;
-			}
-		case EXIT:
-			{
-				cout << INDENT << "Despite all odds, you made it to the exit. Congratulations.\n";
-				gameOver = true;
-				continue;
-			}
+			cout << INDENT << "You are in an empty meadow. There is nothing of note here.\n";
+			break;
 		}
-
-		//List the directions the player can take
-		cout << INDENT << "You can see paths leading to the " <<
-			((playerX > 0) ? "west (4), " : "") <<
-			((playerX < MAZE_WIDTH - 1) ? "east (6), " : "") <<
-			((playerY > 0) ? "north (8), " : "") <<
-			((playerY < MAZE_HEIGHT - 1) ? "south (2), " : "") << "\n";
-
-		cout << INDENT << "Where to now?";
-
-		int x = INDENT_X + (6 * playerX) + 3;
-		int y = MAP_Y + playerY;
-
-		// draw the player's position on the map
-		// move cursor to map pos and delete character at current position
-		cout << CSI << y << ";" << x << "H";
-		cout << MAGENTA << "\x81";
-		// move cursor to position for player to enter input
-
-		cout << CSI << PLAYER_INPUT_Y << ";" << PLAYER_INPUT_X << "H" << YELLOW;
-
-		// clear the input buffer, ready for player input
-		cin.clear();
-		cin.ignore(cin.rdbuf()->in_avail());
-
-		int direction = 0;
-		cin >> direction;
-		cout << RESET_COLOR;
-
-		if (cin.fail())
+	case ENEMY:
 		{
-			continue; // go back to the top of the game loop and ask again
+			cout << INDENT << "BEWARE. An enemy is approaching.\n";
+			break;
 		}
-
-		//Before updating the player position, redraw the old room character over the old position
-		cout << CSI << y << ";" << x << "H";
-		switch (rooms[playerY][playerX])
+	case TREASURE:
 		{
-		case EMPTY:
-			{
-				cout << GREEN << "\xb0" << RESET_COLOR;
-				break;
-			}
-		case ENEMY:
-			{
-				cout << RED << "\x94" << RESET_COLOR;
-				break;
-			}
-		case TREASURE:
-			{
-				cout << YELLOW << "$" << RESET_COLOR;
-				break;
-			}
-		case FOOD:
-			{
-				cout << WHITE << "\xcf" << RESET_COLOR;
-				break;
-			}
-		case ENTRANCE:
-		{
-				cout << WHITE << "\x9d" << RESET_COLOR;
-				break;
-			}
-		case EXIT:
-			{
-				cout << WHITE << "\xFE" << RESET_COLOR;
-			}
+			cout << INDENT << "Your journey has been rewarded. You have found some treasure.\n";
+			break;
 		}
-
-		switch (direction)
+	case FOOD:
 		{
-		case EAST:
-			{
-				if (playerX < MAZE_WIDTH - 1)
-					{
-						playerX++;
-					}
-				break;
+			cout << INDENT << "At last! You collect some food to sustain you on your journey.\n";
+			break;
 		}
-		case WEST:
-			{
-				if (playerX > 0)
-					{
-						playerX--;
-					}
-				break;
-			}
-		case NORTH:
-			{
-				if (playerY > 0)
-					{
-						playerY--;
-					}
-				break;
-			}
-		case SOUTH:
-			{
-				if (playerY < MAZE_HEIGHT - 1)
-					{
-						playerY++;
-					}
-				break;
-			}
-		default:
-			{
-				//Do nothing, go back to the top of the loop and ask again
-				break;
-			}	
+	case ENTRANCE:
+		{
+			cout << INDENT << "The entrance you used to enter this maze is blocked. There is no going back.\n";
+			break;
+		}
+	case EXIT:
+		{
+			cout << INDENT << "Despite all odds, you made it to the exit. Congratulations.\n";
+			break;
 		}
 	}
-	//End of game loop
+}
 
-	cout << "\n" << INDENT << "Press 'Enter' to exit the program.\n";
-	cin.clear();
-	cin.ignore(cin.rdbuf()->in_avail());
+void DrawPlayer(int x, int y)
+{
+	x = INDENT_X + (6 * x) + 3;
+	y = MAP_Y + y;
+
+	//Draw the player's position on the map
+	//Move cursor to map pos and delete character at current position
+	cout << CSI << y << ";" << x << "H";
+	cout << MAGENTA << "\x81" << RESET_COLOR;
+}
+
+void DrawValidDirections(int x, int y)
+{
+	//Reset draw colours
+	cout << RESET_COLOR;
+
+	//Jump to the correct location
+	cout << CSI << MOVEMENT_DESC_Y + 1 << ";" << 0 << "H";
+	cout << INDENT << "You can see paths leading to the " <<
+		((x > 0) ? "west, " : "") <<
+		((x < MAZE_WIDTH - 1) ? "east, " : "") <<
+		((y > 0) ? "north, " : "") <<
+		((y < MAZE_HEIGHT - 1) ? "south, " : "") << "\n";
+}
+
+int GetMovementDirection()
+{
+	//Jump to the correct location
+	cout << CSI << PLAYER_INPUT_Y << ";" << 0 << "H";
+	cout << INDENT << "Where to now?";
+
+	int direction;
+
+	//Move cursor to position for player to enter input
+	cout << CSI << PLAYER_INPUT_Y << ";" << PLAYER_INPUT_X << "H" << YELLOW;
+
+	//Clear the input buffer, ready for player input
 	cin.get();
+	cin.ignore(cin.rdbuf()->in_avail());
 
-	return;
+	cin >> direction;
+	cout << RESET_COLOR;
+
+	if (cin.fail())
+	{
+		return 0;
+	}
+
+	return direction;
 }
